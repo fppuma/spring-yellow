c6be51ae99d30f9aba2b987a7c687ea4a69172bc
Francisco Puma <fppuma@gmail.com>
2023-04-09 09:24:42 -0500

    initial commit

---
+++ src/test/java/com/example/web/controller/HelloControllerUnitTest.java
@@ -0,0 +1,20 @@
+package com.example.web.controller;
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+import org.springframework.validation.support.BindingAwareModelMap;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloControllerUnitTest {
+  @Test
+  public void getMessage() {
+      HelloController controller = new HelloController();
+      Model model = new BindingAwareModelMap();
+      String result = controller.getMessage("World", model);
+      assertAll(
+              () -> assertEquals("World", model.getAttribute("user")),
+              () -> assertEquals("welcome", result)
+      );
+  }
+}
