9c1875124eb75d83243665da8499183c6d14c296
Francisco Puma <fppuma@gmail.com>
2023-04-09 11:37:05 -0500

    update readme and tab-space

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # spring-yellow
 Spring 3 and Java 17
+
+## Starters
+```xml
+<dependency>
+  <groupId>org.springframework.boot</groupId>
+  <artifactId>spring-boot-starter-thymeleaf</artifactId>
+</dependency>
+
+<dependency>
+  <groupId>org.springframework.boot</groupId>
+  <artifactId>spring-boot-starter-web</artifactId>
+</dependency>
+``` 
+
+## Logger
+In the application.properties
+```
+logging.level.web=debug
+```
--- src/test/java/com/example/web/controller/HelloControllerUnitTest.java
+++ src/test/java/com/example/web/controller/HelloControllerUnitTest.java
@@ -9,12 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class HelloControllerUnitTest {
   @Test
   public void getMessage() {
-      HelloController controller = new HelloController();
-      Model model = new BindingAwareModelMap();
-      String result = controller.getMessage("World", model);
-      assertAll(
-              () -> assertEquals("World", model.getAttribute("user")),
-              () -> assertEquals("welcome", result)
-      );
+    HelloController controller = new HelloController();
+    Model model = new BindingAwareModelMap();
+    String result = controller.getMessage("World", model);
+    assertAll(
+            () -> assertEquals("World", model.getAttribute("user")),
+            () -> assertEquals("welcome", result)
+    );
   }
 }
